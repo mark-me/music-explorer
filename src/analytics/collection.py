97df6516a07e3a42b0b1e210e4832c38bd623f09
Mark Zwart <mark.zwart@pobox.com>
2025-02-19 00:31:51 +0100

    Added track lists

--- src/analytics/collection.py
+++ src/analytics/collection.py
@@ -1,4 +1,7 @@
+import polars as pl
+
 from db_operations import DBStorage
+
 
 class Collection(DBStorage):
     def __init__(self, file_db, schema="main"):
@@ -43,8 +46,36 @@ class Collection(DBStorage):
 
     def artist(self, id_artist: str) -> list:
         sql = self.sql_all + f" AND a.id_artist={id_artist} ORDER BY ci.year_released"
-        lst_dicts = self.read_sql(sql=sql).to_dicts()
-        #lst_items = self.read_sql(sql=sql).to_dicts()
-        #for i, item in enumerate(lst_items):
+        df_releases = self.read_sql(sql=sql)
+        lst_release_id = pl.Series(df_releases.select("id_release")).to_list()
+        lst_dicts = df_releases.to_dicts()
+
+        # Retrieve tracks
+        lst_release_id = [str(i) for i in lst_release_id]
+        str_release_ids = ", ".join(lst_release_id)
+        sql = f"""
+        SELECT
+            rt.id_release,
+            rt.position,
+            rt.title
+        FROM release_tracks rt
+        WHERE rt.id_release IN ({str_release_ids} )
+        """
+        lst_tracks = self.read_sql(sql=sql).to_dicts()
+        dict_tracks = {}
+        for track in lst_tracks:
+            id_release = str(track["id_release"])
+            if id_release not in dict_tracks:
+                dict_tracks.update(
+                    {id_release: [{"position": track["position"], "title": track["title"]}]}
+                )
+            else:
+                dict_tracks[id_release].append(
+                    {"position": track["position"], "title": track["title"]}
+                )
+
+        # Embed tracks in releases
+        for i, release in enumerate(lst_dicts):
+            lst_dicts[i].update({"tracks": dict_tracks[str(release["id_release"])]})
 
         return lst_dicts
