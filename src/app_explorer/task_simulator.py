9edcb2eef3c5c2958521fa3edc8060247bfca930
Mark Zwart <mark.zwart@pobox.com>
2025-03-12 22:22:09 +0100

    removed logging

--- src/app_explorer/task_simulator.py
+++ src/app_explorer/task_simulator.py
@@ -1,22 +1,12 @@
-import logging
 import random
 import time
 
-from celery.utils.log import get_task_logger
-
-logger = get_task_logger(__name__)
-logger.setLevel(logging.INFO)  # Set logging level to INFO
-handler = logging.FileHandler("my_log.log")
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
 class TaskSimulator:
     def __init__(self, celery_app):
         self.celery = celery_app
 
     def start(self):
-        logger.info("Starting steps")
         self.celery.update_state(
                 state="PROGRESS", meta={"step": "start", "current": 0, "total": 0, "item": ""}
             )
@@ -33,7 +23,6 @@ class TaskSimulator:
         self.celery.update_state(
                 state="PROGRESS", meta={"step": step, "current": i, "total": total, "item": ""}
             )
-        logger.info(step)
         time.sleep(1)
         i = i + 1
         self.celery.update_state(
@@ -47,13 +36,11 @@ class TaskSimulator:
         noun = ["solar array", "particle reshaper", "cosmic ray", "orbiter", "bit"]
         message = ""
         total = random.randint(5, 12)
-        logger.info("Collection items")
         for i in range(total):
             if not message or random.random() < 0.25:
                 message = "{0} {1}...".format(
                     random.choice(verb), random.choice(adjective)
                 )
-                logger.info(f"Collection item {message}")
             self.celery.update_state(
                 state="PROGRESS", meta={"step": "Collection items", "current": i, "total": total, "item": message}
             )
@@ -67,5 +54,4 @@ class TaskSimulator:
             self.celery.update_state(
                 state="PROGRESS", meta={"step": "Collection artists", "current": i, "total": total, "item": message}
             )
-            logger.info(f"Artist {message}")
             time.sleep(1)
