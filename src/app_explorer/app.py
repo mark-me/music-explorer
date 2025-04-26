a67c4bbb7b7c35bcf5dee93fd69828184cb9580b
Mark Zwart <mark.zwart@pobox.com>
2025-04-27 00:00:53 +0200

    Added Docstrings

--- src/app_explorer/app.py
+++ src/app_explorer/app.py
@@ -49,32 +49,48 @@ def celery_tasks_running() -> str:
 
 @app.route("/manifest.json")
 def serve_manifest():
+    """Serves the web app manifest file.
+
+    This route serves the `manifest.json` file, which provides metadata about the web application.
+    """
     return app.send_static_file("manifest.json", mimetype="application/manifest+json")
 
 
 @app.route("/service-worker.js")
 def serve_sw():
+    """Serves the service worker file.
+
+    This route serves the `service-worker.js` file, which handles caching and offline functionality
+    for the web application.
+    """
     return app.send_static_file("service-worker.js")
 
 
 @app.route("/offline.html")
 def offline():
+    """Serves the offline page.
+
+    This route serves the `offline.html` file, which is displayed when the web application
+    is offline.
+    """
     return app.send_static_file("offline.html")
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    """Home page"""
+    """Home page.
+
+    This route renders the home page of the application.
+    """
     return render_template("home.html")
 
 
 @app.route("/artists")
 def artists():
-    """Route for artists
+    """Artists page.
 
-    Returns:
-        _type_: Renders the page showing 5 random artists, and the first 10 of all artists
+    This route renders the artists page, displaying a selection of random artists and the top 10 artists.
     """
     db_artists = Artists(file_db=file_db)
     lst_random = db_artists.random(qty_sample=5)
@@ -86,10 +102,9 @@ def artists():
 
 @app.route("/artists_all")
 def artists_all():
-    """Route for all artists
+    """All artists page.
 
-    Returns:
-        _type_: Renders the page showing all artists
+    This route renders the all artists page, displaying all artists from the database.
     """
     db_artists = Artists(file_db=file_db)
     lst_artists = db_artists.all()
@@ -98,6 +113,11 @@ def artists_all():
 
 @app.route("/artists_search")
 def artists_search():
+    """Searches for artists.
+
+    This route handles artist searches, querying the database based on the provided query parameter.
+    If no query is provided, it returns all artists.
+    """
     query = request.args.get("query")
     db_artists = Artists(file_db=file_db)
     lst_artists = db_artists.search(query) if query else db_artists.all()
@@ -106,6 +126,14 @@ def artists_search():
 
 @app.route("/artist/<int:id_artist>")
 def artist(id_artist):
+    """Artist details page.
+
+    This route renders the artist details page, displaying information about a specific artist
+    and their releases in the user's collection.
+
+    Args:
+        id_artist (int): The ID of the artist.
+    """
     db_collection = Collection(file_db=file_db)
     artists = Artists(file_db=file_db)
     dict_artist = artists.artist(id_artist=id_artist)
@@ -120,7 +148,11 @@ def artist(id_artist):
 
 @app.route("/collection_items")
 def collection_items():
-    """Collection"""
+    """Collection items page.
+
+    This route renders the main collection items page, displaying a selection of random items
+    and the top 10 items from the user's collection.
+    """
     collection = Collection(file_db=file_db)
     lst_random = collection.random(qty_sample=5)
     lst_all = collection.all_top_10()
@@ -134,7 +166,10 @@ def collection_items():
 
 @app.route("/collection_items_all")
 def collection_items_all():
-    """Collection"""
+    """All collection items page.
+
+    This route renders the all collection items page, displaying all items from the database.
+    """
     collection = Collection(file_db=file_db)
     lst_all = collection.all()
     return render_template(
@@ -146,6 +181,11 @@ def collection_items_all():
 
 @app.route("/collection_items_search")
 def collection_items_search():
+    """Searches for collection items.
+
+    This route handles searches within the user's collection, querying the database based on
+    the provided query parameter. If no query is provided, it returns all collection items.
+    """
     query = request.args.get("query")
     db_collection = Collection(file_db=file_db)
     lst_all = db_collection.search(query) if query else db_collection.all()
@@ -156,12 +196,24 @@ def collection_items_search():
 
 @app.route("/collection_item/<int:id_release>")
 def collection_item(id_release: int):
+    """Collection item details page.
+
+    This route renders the collection item details page for a given release ID.
+
+    Args:
+        id_release (int): The ID of the release.
+    """
     release = Release(id_release=id_release, file_db=file_db).data()
     return render_template("collection_items/collection_item.html", item=release)
 
 
 @app.route("/config")
 def config_page():
+    """Configuration page.
+
+    This route renders the configuration page, allowing the user to manage Discogs credentials
+    and initiate the Discogs data extraction process.
+    """
     url = os.environ("MUSIC_EXPLORER_URL", config['url'])
     dict_config = {
         "credentials_ok": discogs.check_user_tokens(),
@@ -172,25 +224,37 @@ def config_page():
 
 @app.route("/receive-token", methods=["GET"])
 def accept_user_token():
-    """Callback function to process the user authentication result"""
+    """Handles the Discogs OAuth token callback.
+
+    This route receives the OAuth verifier from Discogs, saves the user token,
+    and redirects to the configuration page.
+    """
+
     discogs.save_user_token(request.args["oauth_verifier"])
     return redirect(url_for("config_page"))
 
 
 @app.route("/discogs_etl")
 def start_ETL():
+    """Starts the Discogs ETL task.
+
+    This route triggers the Discogs ETL process using Celery.
+    If a task is already running, it returns the existing task ID.
+    """
     global id_task_etl
     id_task_etl = celery_tasks_running()
     if not id_task_etl:
         task = discogs_etl.delay()
         id_task_etl = task.id
-        return jsonify({"success": True, "task_id": id_task_etl})
-    else:
-        return jsonify({"success": True, "task_id": id_task_etl})
+    return jsonify({"success": True, "task_id": id_task_etl})
 
 
 @app.route("/task_etl_id")
 def get_task_ETL_id():
+    """Retrieves the ID of the current ETL task.
+
+    This route returns the ID of the currently running ETL task, or None if no task is active.
+    """
     global id_task_etl
     id_task_etl = celery_tasks_running()
     return jsonify({"success": True, "task_id": id_task_etl})
@@ -198,6 +262,11 @@ def get_task_ETL_id():
 
 @app.route("/simulate_etl")
 def start_simulate_ETL():
+    """Starts a simulated ETL task.
+
+    This route initiates a simulated ETL task using Celery and redirects to the configuration page.
+    If a task is already running, it reuses the existing task ID.
+    """
     global id_task_etl
     id_task_etl = celery_tasks_running()
     if not id_task_etl:
@@ -208,6 +277,13 @@ def start_simulate_ETL():
 
 @app.route("/check_task/<task_id>", methods=["GET"])
 def check_task(task_id):
+    """Checks the status of a Celery task.
+
+    This route retrieves the status and result of a Celery task by its ID.
+
+    Args:
+        task_id: The ID of the Celery task.
+    """
     task = celery_app.AsyncResult(task_id)
     task_status = {"status": task.state}
     with contextlib.suppress(TypeError):
@@ -218,7 +294,10 @@ def check_task(task_id):
 
 @app.route("/about")
 def about():
-    """About page"""
+    """About page.
+
+    This route renders the about page of the application.
+    """
     return render_template("about.html", title="About")
 
 
--- src/utils/secrets_yaml.py
+++ src/utils/secrets_yaml.py
@@ -1,36 +1,52 @@
 import yaml
 import os
 
-class SecretsYAML():
+
+class SecretsYAML:
+    """Manages secrets stored in a YAML file.
+
+    This class provides methods for reading, writing, and validating secrets
+    stored in a YAML file, specific to a given application.
+    """
     def __init__(self, file_path: str, app: str, expected_keys: set) -> None:
+        """Initializes SecretsYAML with file path, app name, and expected keys.
+
+        Args:
+            file_path (str): The path to the YAML file.
+            app (str): The name of the application.
+            expected_keys (set): A set of expected keys for the application's secrets.
+        """
         self._file = file_path
         self.__create_path()
-
         self._app = app
         self._expected_keys = expected_keys
 
     def __create_path(self) -> None:
+        """Creates the directory path for the secrets file if it doesn't exist."""
         path = os.path.dirname(self._file)
         isExist = os.path.isdir(path)
-        if not isExist:   # Create a new directory because it does not exist
+        if not isExist:  # Create a new directory because it does not exist
             os.makedirs(path)
 
-    def is_complete(self):
-        """See is all expected fields are present in the secrets file
+    def is_complete(self) -> tuple[bool, str]:
+        """Checks if the secrets file is complete.
+
+        This method checks if the secrets file exists, if it contains settings for the specified app,
+        and if all expected keys are present within the app's settings.
 
         Returns:
-            dict: A status code and description of the test
+            tuple[bool, str]: A tuple containing a boolean indicating whether the file is complete
+                and a string describing the validation result.
         """
         # Check file existence
         if not os.path.isfile(self._file):
-            return False, 'There is no config file for secrets in ' + self._file
+            return False, f"There is no config file for secrets in {self._file}"
         # Load the YAML file
-        with open(self._file, 'r') as file:
+        with open(self._file, "r") as file:
             try:
                 yaml_data = yaml.safe_load(file)
             except yaml.YAMLError as e:
-                return {'status_code': 500,
-                        'detail': f"Error loading file: {str(e)}"}
+                return {"status_code": 500, "detail": f"Error loading file: {str(e)}"}
 
         # Check if there are any app settings
         if self._app not in yaml_data.keys():
@@ -39,36 +55,46 @@ class SecretsYAML():
         # Check if there are apps
         missing_keys = self._expected_keys - set(yaml_data[self._app].keys())
         if missing_keys:
-            return False, f"Missing keys {', '.join(missing_keys)} for {self._app} in file {self._file}"
+            return (
+                False,
+                f"Missing keys {', '.join(missing_keys)} for {self._app} in file {self._file}",
+            )
 
-        return True, 'YAML file is valid'
+        return True, "YAML file is valid"
 
     def write_secrets(self, dict_secrets: dict) -> None:
-        """Write a secrets on an app to a YAML file
+        """Writes secrets for an app to the YAML file.
+
+        This method writes or updates the secrets for the specified application in the YAML file.
+        If the file doesn't exist, it creates a new one. If the app's section doesn't exist,
+        it creates it.
 
         Args:
-            dict_secrets (dict): dictionary containing the secrets
+            dict_secrets (dict): A dictionary containing the secrets to write.
         """
         try:
-            with open(self._file, 'r') as file:
+            with open(self._file, "r") as file:
                 yaml_data = yaml.safe_load(file)
         except IOError:
             yaml_data = {}
-            with open(self._file, 'w') as file:
+            with open(self._file, "w") as file:
                 pass
         yaml_data[self._app] = dict_secrets
-        with open(self._file, 'w') as file:
+        with open(self._file, "w") as file:
             yaml.safe_dump(yaml_data, file)
 
     def read_secrets(self) -> dict:
-        """Read an app's secrets
+        """Reads the secrets for the app from the YAML file.
+
+        This method reads and returns the secrets associated with the specified application.
+        It first checks if the secrets file is complete. If not, it returns None.
 
         Returns:
-            dict: All secrets
+            dict: A dictionary containing the app's secrets, or None if the file is incomplete.
         """
         is_complete, value = self.is_complete()
         if is_complete:
-            with open(self._file, 'r') as file:
+            with open(self._file, "r") as file:
                 yaml_data = yaml.safe_load(file)
             return yaml_data[self._app]
         else:
