6359822be6f513c031144aa775ee52322fcc6bc9
Mark Zwart <mark.zwart@pobox.com>
2025-04-26 23:10:27 +0200

    Added Docstrings

--- src/utils/manage_db.py
+++ src/utils/manage_db.py
@@ -5,32 +5,58 @@ import time
 from .backup_cleaner import BackupCleaner
 
 class ManageDB:
+    """Manages database operations, including copying, replacing, and backing up.
+
+    This class provides methods for creating copies of the database for loading data,
+    replacing the original database with the loaded copy, and creating backups of the database.
+    """
     def __init__(self, file_db: str) -> None:
+        """Initializes ManageDB with the database file path.
+
+        Args:
+            file_db (str): The path to the database file.
+        """
         self._db_path = file_db
         self._db_dir = os.path.dirname(file_db)
         filename, self.__db_ext = os.path.splitext(file_db)
         self._db_file = os.path.basename(filename)
-        self._db_load = filename + '_load' +  self.__db_ext
+        self._db_load = f'{filename}_load{self.__db_ext}'
         self._exists = os.path.exists(self._db_path)
 
     def create_load_copy(self) -> str:
-        """Create a copy of the database for loading and return it's location"""
+        """Creates a copy of the database for loading data.
+
+        Returns:
+            str: The path to the newly created database copy.
+        """
         if self._exists:
             shutil.copyfile(self._db_path, self._db_load)
         return self._db_load
 
     def replace_db(self) -> None:
-        """Replace the database with the copy used for loading"""
+        """Replaces the original database with the loaded copy.
+
+        This method copies the contents of the load database to the original database path
+        and then removes the load database file.
+        """
         shutil.copyfile(self._db_load, self._db_path)
         os.remove(self._db_load)
 
     def create_backup(self) -> None:
-        """Create a backup of the database"""
+        """Creates a backup of the database.
+
+        This method creates a timestamped backup of the database file in a 'backup' directory
+        within the database's directory. Old backups are cleaned up based on a retention policy.
+        """
         if self._exists:
-            dir_backup = self._db_dir + '/backup'
+            dir_backup = f'{self._db_dir}/backup'
             if not os.path.exists(dir_backup):
                 os.makedirs(dir_backup)
-            file_backup = dir_backup + '/' + self._db_file + '_' + time.strftime("%Y%m%d_%H%M%S") + '.db'
+            file_backup = (
+                f'{dir_backup}/{self._db_file}_'
+                + time.strftime("%Y%m%d_%H%M%S")
+                + '.db'
+            )
             shutil.copyfile(self._db_path, file_backup)
             # TODO: Add retention policy
             #backup_cleaner = BackupCleaner(dir_backup=dir_backup)
